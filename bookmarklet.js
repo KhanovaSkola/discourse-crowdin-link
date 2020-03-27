b3056f033fab4a0bd1377e75f1cfdb0459c8dbfb
Daniel Hollas <danekhollas@gmail.com>
2020-03-27 21:00:43 +0100

    Fix bookmarklet for Caroline

--- bookmarklet.js
+++ bookmarklet.js
@@ -3,7 +3,7 @@
 //
 // These strings are then localized per user by this theme component
 javascript: (function() {
-  var crowdinUrlRegex = /(http[s]?:\/\/[^/]+\/translate\/khanacademy\/[^/]+)\/enus-[^#]+(#.*)/;
+  var crowdinUrlRegex = /(http[s]?:\/\/[^/]+\/translate\/khanacademy\/[^/]+)\/enus-[^#]+(#.*)/g;
   var editor = document.getElementsByClassName("d-editor-input")[0];
   var replaced = editor.value.replace(crowdinUrlRegex, '$1/enus-XX$2');
   editor.value = replaced;
